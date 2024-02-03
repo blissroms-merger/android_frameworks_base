f8bfd68d9fb829e22c92c49d67e90378e096fd9d
minaripenguin <minaripenguin@users.noreply.github.com>
2024-02-04 07:40:28 +0800

    services: Refactor FIFO UI scheduling
    test:
    adb shell ps -lT <pid>, check thread priorities
    adb shell 'cat /proc/$(pidof app name)/cgroup'
    
    Change-Id: I6c67a2df1c84a38b3ee962bbda636ca433e53c9b
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/am/OomAdjuster.java
+++ services/core/java/com/android/server/am/OomAdjuster.java
@@ -3089,13 +3089,7 @@ public class OomAdjuster {
                 mProcessGroupHandler.sendMessage(mProcessGroupHandler.obtainMessage(
                         0 /* unused */, app.getPid(), processGroup, app.processName));
                 try {
-                    if (oldSchedGroup != curSchedGroup) {
-                        int decremented = --uidRec.numSchedGroup[oldSchedGroup];
-                        int incremented = ++uidRec.numSchedGroup[curSchedGroup];
-                        if (decremented == 0 || incremented == 1) {
-                            mService.updateCgroupPrioLocked(uidRec);
-                        }
-                    }
+                    mService.updateCgroupPrioLocked(app.uid, app.getPid());
                     final int renderThreadTid = app.getRenderThreadTid();
                     if (curSchedGroup == SCHED_GROUP_TOP_APP) {
                         // do nothing if we already switched to RT
